bdc1717fee059c4c5ee6d3fb190ee7c68a6a736b
Axionize <154778082+Axionize@users.noreply.github.com>
2025-04-20 08:39:04 -0400

    Add /grim history command to see historical alerts
    - Add basic runtime reloading support.
      - Will only actually reload if current database is different/has different setting than the one requested in config.
    
    - Added [log] statements matching [alert] statements

--- fabric/src/main/java/ac/grim/grimac/platform/fabric/GrimACFabricLoaderPlugin.java
+++ fabric/src/main/java/ac/grim/grimac/platform/fabric/GrimACFabricLoaderPlugin.java
@@ -5,7 +5,6 @@ import ac.grim.grimac.GrimAPI;
 import ac.grim.grimac.api.GrimAPIProvider;
 import ac.grim.grimac.api.plugin.GrimPlugin;
 import ac.grim.grimac.platform.api.PlatformLoader;
-import ac.grim.grimac.platform.api.PlatformServer;
 import ac.grim.grimac.platform.api.manager.*;
 import ac.grim.grimac.platform.api.sender.Sender;
 import ac.grim.grimac.platform.api.sender.SenderFactory;
@@ -58,7 +57,7 @@ public abstract class GrimACFabricLoaderPlugin implements PlatformLoader {
 
     protected final ParserDescriptorFactory parserFactory;
     protected final FabricPlatformPlayerFactory playerFactory;
-    protected final PlatformServer platformServer;
+    protected final AbstractFabricPlatformServer platformServer;
     @Getter
     protected final IFabricConversionUtil fabricConversionUtil;
     protected final IFabricMessageUtil fabricMessageUtil;
@@ -66,7 +65,7 @@ public abstract class GrimACFabricLoaderPlugin implements PlatformLoader {
     public GrimACFabricLoaderPlugin(
                                     ParserDescriptorFactory parserDescriptorFactory,
                                     FabricPlatformPlayerFactory playerFactory,
-                                    PlatformServer platformServer,
+                                    AbstractFabricPlatformServer platformServer,
                                     IFabricMessageUtil fabricMessageUtil,
                                     IFabricConversionUtil fabricConversionUtil
     ) {
@@ -149,7 +148,7 @@ public abstract class GrimACFabricLoaderPlugin implements PlatformLoader {
     }
 
     @Override
-    public PlatformServer getPlatformServer() {
+    public AbstractFabricPlatformServer getPlatformServer() {
         return platformServer;
     }
 
